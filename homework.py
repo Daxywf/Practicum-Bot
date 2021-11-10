ba1a9dd35de9e76af3d4211c2009f8c545637e3e
Dmitry Deev <deeff230804@mail.ru>
2021-11-10 11:49:53 +0300

    4th commit: new tests

--- homework.py
+++ homework.py
@@ -4,21 +4,21 @@ import time
 from logging.handlers import RotatingFileHandler
 
 import requests
+import telegram
 from dotenv import load_dotenv
 from requests.exceptions import RequestException
-from telegram import Bot
-import telegram
 
 load_dotenv()
 
+
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-CHAT_ID = os.getenv('CHAT_ID')
+TELEGRAM_CHAT_ID = os.getenv('CHAT_ID')
 RETRY_TIME = 60 * 10
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена, в ней нашлись ошибки.'
+    'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 HEADERS = {'Authorization': PRACTICUM_TOKEN}
@@ -49,6 +49,8 @@ ERROR_MESSAGE = 'Сбой в работе программы: {error}'
 SEND_ERROR = 'Боту не удалось отправить сообщение. Ошибка: {error}'
 MESSAGE_SENT = 'Бот отправил сообщение: {message}'
 UNEXPECTED_STATUS = 'Неожиданный статус: {status}'
+NO_KEY = 'Отсутствует ключ: {key}}'
+RESPONSE_NOT_DICT = 'Ответ не является словарём'
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -69,17 +71,18 @@ logger.addHandler(console_out)
 
 def send_message(bot, message):
     """Отправляет сообщение пользователю в Telegram."""
-    bot.send_message(CHAT_ID, message)
+    bot.send_message(TELEGRAM_CHAT_ID, message)
     logger.info(MESSAGE_SENT.format(message=message))
 
 
-def get_api_answer(url, current_timestamp):
+def get_api_answer(current_timestamp):
     """Получает ответ от API Практикума."""
-    payload = {'from_date': current_timestamp}
+    timestamp = current_timestamp or int(time.time())
+    params = {'from_date': timestamp}
     request_parameters = dict(
         url=ENDPOINT,
         headers=HEADERS,
-        params=payload
+        params=params
     )
     try:
         response = requests.get(**request_parameters)
@@ -91,16 +94,17 @@ def get_api_answer(url, current_timestamp):
             )
         )
     answer = response.json()
-    for key in answer.keys():
-        if key == 'code' or key == 'error':
-            raise RequestException(
-                SERVER_ERROR.format(
-                    error=answer['error'],
-                    sevrer_code=answer['code'],
-                    code=response.status_code,
-                    **request_parameters
+    if isinstance(answer, dict):
+        for key in answer.keys():
+            if key == 'code' or key == 'error':
+                raise RequestException(
+                    SERVER_ERROR.format(
+                        error=answer['error'],
+                        sevrer_code=answer['code'],
+                        code=response.status_code,
+                        **request_parameters
+                    )
                 )
-            )
     if response.status_code != 200:
         raise RequestException(
             CODE_IS_NOT_200.format(
@@ -111,46 +115,62 @@ def get_api_answer(url, current_timestamp):
     return answer
 
 
-def parse_status(homework):
-    """Получает последнюю работу и формирует сообщение пользователю."""
-    return NEW_STATUS.format(
-        homework_name=homework['homework_name'],
-        verdict=VERDICTS[homework['status']]
-    )
-
-
 def check_response(response):
     """Анализирует ответ API и возвращает последнюю домашнюю работу."""
+    if not isinstance(response, dict):
+        raise TypeError(RESPONSE_NOT_DICT)
     homeworks = response['homeworks']
     homework = homeworks[0]
-    status = homework['status']
-    if status not in VERDICTS:
-        raise ValueError(UNEXPECTED_STATUS.format(status=status))
     return homework
 
 
-def main():
-    """Запускает работу бота."""
-    for name in ('PRACTICUM_TOKEN', 'TELEGRAM_TOKEN', 'CHAT_ID'):
+def parse_status(homework):
+    """Получает последнюю работу и формирует сообщение пользователю."""
+    keys = ['homework_name', 'status']
+    for key in keys:
+        if key not in homework:
+            raise ValueError(NO_KEY.format(key))
+    status = homework['status']
+    if status not in VERDICTS:
+        raise ValueError(UNEXPECTED_STATUS.format(status=status))
+    return NEW_STATUS.format(
+        homework_name=homework['homework_name'],
+        verdict=VERDICTS[status]
+    )
+
+
+def check_tokens():
+    """Проверяет наличие основных токенов."""
+    for name in ('PRACTICUM_TOKEN', 'TELEGRAM_TOKEN', 'TELEGRAM_CHAT_ID'):
         if globals()[name] is None:
             logger.critical(MISSING_ENV_VARS.format(variable=name))
-            raise NameError(MISSING_ENV_VARS.format(variable=name))
-    bot = Bot(token=TELEGRAM_TOKEN)
-    while True:
-        try:
-            current_timestamp = int(time.time())
-            answer = get_api_answer(ENDPOINT, current_timestamp - RETRY_TIME)
-            homework = check_response(answer)
-            verdict = parse_status(homework)
-            send_message(bot, verdict)
-        except Exception as error:
-            message = ERROR_MESSAGE.format(error=error)
-            logger.error(message)
+            return False
+        return True
+
+
+def main():
+    """Основная логика работы бота."""
+    if check_tokens() is False:
+        raise NameError()
+    else:
+        bot = telegram.Bot(token=TELEGRAM_TOKEN)
+        current_timestamp = int(time.time())
+        while True:
             try:
-                send_message(bot, message)
-            except telegram.TelegramError as error:
-                logger.error(SEND_ERROR.format(error=error))
-        time.sleep(RETRY_TIME)
+                current_timestamp = int(time.time())
+                answer = get_api_answer(ENDPOINT, current_timestamp)
+                homework = check_response(answer)
+                verdict = parse_status(homework)
+                send_message(bot, verdict)
+                current_timestamp = current_timestamp - RETRY_TIME
+            except Exception as error:
+                message = ERROR_MESSAGE.format(error=error)
+                logger.error(message)
+                try:
+                    send_message(bot, message)
+                except telegram.TelegramError as error:
+                    logger.error(SEND_ERROR.format(error=error))
+            time.sleep(RETRY_TIME)
 
 
 if __name__ == '__main__':
